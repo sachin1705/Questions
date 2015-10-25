db0e856e5fb7010db48369fb9f5b48574d2c11d6
sachin1705 <sachintalreja17@gmail.com>
2015-10-26 02:44:39 +0530

    HelloWorld Project
    Added from Windows Git

---
+++ HelloWord/src/HelloWord.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : HelloWord.cpp
+// Author      : Sachin
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include<stack>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
